5fb731a7598afb548c23241156608a87fb484f08
RickDeuS <riosr3060@gmail.com>
2023-06-29 16:28:17 -0500

    jwt funcionando

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ require('dotenv').config();
 const app = express();
 
 // Conexión a Base de datos
-const uri = `mongodb+srv://riosr3060:${process.env.PASSWORD}@denuncias-back.eugamd3.mongodb.net/${process.env.DBNAME}`;
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@denuncias-back.eugamd3.mongodb.net/${process.env.DBNAME}`;
 
 mongoose
-    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Se estableció conexión con la base de datos'))
-    .catch((e) => console.log('Error de conexión a la base de datos:', e));
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Se estableció conexión con la base de datos'))
+  .catch((e) => console.log('Error de conexión a la base de datos:', e));
 
 // Capturar body
 app.use(bodyparser.urlencoded({ extended: false }));
@@ -21,19 +21,26 @@ const authRoutes = require('./src/Routes/auth');
 const homeRoutes = require('./src/Routes/home');
 const verifyToken = require('./src/Routes/validate-token');
 
-// Rutas middleware
-app.use('/home', verifyToken, homeRoutes);
+// Ruta de autenticación
 app.use('/auth', authRoutes);
 
+// Middleware para verificar el token en las rutas / y /home
+app.use('/', verifyToken);
+app.use('/home', verifyToken);
+
+// Ruta por defecto
 app.get('/', (req, res) => {
-    res.json({
-        estado: true,
-        mensaje: '¡Funciona!',
-    });
+  res.json({
+    estado: true,
+    mensaje: '¡Funciona!',
+  });
 });
+
+// Ruta protegida /home
+app.use('/home', homeRoutes);
 
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor en ejecución en el puerto: ${PORT}`);
+  console.log(`Servidor en ejecución en el puerto: ${PORT}`);
 });
