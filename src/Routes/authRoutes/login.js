ab209089d9cfbac5088aa6b336677a0743cb817d
RickDeuS <riosr3060@gmail.com>
2023-08-08 00:19:43 -0500

    login

--- src/Routes/authRoutes/login.js
+++ src/Routes/authRoutes/login.js
@@ -4,10 +4,6 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Joi = require('@hapi/joi');
 
-const schemaLogin = Joi.object({
-    email: Joi.string().min(6).max(255).required().email(),
-    password: Joi.string().min(6).max(1024).required(),
-});
 
 /**
  * @swagger
@@ -75,9 +71,6 @@ const schemaLogin = Joi.object({
 
 router.post('/', async (req, res) => {
     // Validaciones
-    const { error } = schemaLogin.validate(req.body);
-    if (error) return res.status(400).json({ error: error.details[0].message });
-
     const user = await User.findOne({ email: req.body.email });
     if (!user) return res.status(400).json({ error: 'Usuario no encontrado' });
 
