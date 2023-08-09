e32f5d2262a2e8e1d743ca03c8f43ddc13dc4278
RickDeuS <riosr3060@gmail.com>
2023-08-09 02:05:48 -0500

    expires token

--- src/Middleware/validate-token.js
+++ src/Middleware/validate-token.js
@@ -5,7 +5,7 @@ const verifyToken = (req, res, next) => {
     const token = req.header('auth-token');
     if (!token) return res.status(401).json({ error: 'Acceso denegado' });
     try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET, { expiresIn: '2h' });
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET, { expiresIn: '1h' });
         req.user = verified;
         next();
     } catch (error) {
