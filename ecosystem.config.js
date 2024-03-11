b73da2985470c1995e7bde6fbb6e464e4b2628fa
RicardoRios <riosr3060@gmail.com>
2024-03-11 11:18:18 -0500

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: 'BACKEND-PUYANGO:3085',
-        script: 'app.js',
+        script: 'index.js',
         watch: true,
         autorestart: true,
         restart_delay: 5000 
