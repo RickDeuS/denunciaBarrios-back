b3fa2ef0d4bc267b22015f6a0fd1e8951cf7f911
RicardoRios <riosr3060@gmail.com>
2024-03-12 12:47:38 -0500

    logs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,13 @@ module.exports = {
     apps: [{
         name: 'BACKEND-PUYANGO:3085',
         script: 'index.js',
-        
-        // Escalabilidad y Balanceo de Carga
-        instances: 'max', 
-        exec_mode: 'cluster', 
 
         // Gestión Avanzada de Reinicios
         watch: true, 
         ignore_watch: ['node_modules', 'logs', 'err.log', 'out.log'], 
         autorestart: true, 
         restart_delay: 30000, 
-        max_restarts: 100, 
+        max_restarts: 10, 
         exp_backoff_restart_delay: 100, 
 
         // Manejo de Logs
