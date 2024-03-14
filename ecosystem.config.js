a2e837b40422cb8187bebcffcf40736155883323
RicardoRios07 <riosr3060@gmail.com>
2024-03-14 00:23:09 -0500

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,18 +4,18 @@ module.exports = {
         script: 'index.js',
 
         // Gestión Avanzada de Reinicios
-//        watch: false, 
-//      ignore_watch: ['node_modules', 'logs', 'err.log', 'out.log'], 
-//        autorestart: false, 
-//        restart_delay: 30000, 
-//        max_restarts: 10, 
-//        exp_backoff_restart_delay: 100, 
+        watch: false, 
+        ignore_watch: ['node_modules', 'logs', 'err.log', 'out.log', 'src'], 
+        autorestart: false, 
+        restart_delay: 30000, 
+        max_restarts: 10, 
+        exp_backoff_restart_delay: 100, 
 
         // Manejo de Logs
-//        log_date_format: "YYYY-MM-DD HH:mm:ss",
-//        combine_logs: true, 
-//        error_file: 'err.log', 
-//        out_file: 'out.log', 
-//        merge_logs: true 
+        log_date_format: "YYYY-MM-DD HH:mm:ss",
+        combine_logs: true, 
+        error_file: 'err.log', 
+        out_file: 'out.log', 
+        merge_logs: true 
     }]
 };
